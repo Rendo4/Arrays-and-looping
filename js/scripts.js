c9a202f3f0b47f91f47c96c1579bffb806314ad9
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-06-20 00:33:37 -0700

    added else if statements to buisness logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,32 @@ numbers = []
 
 $(document).ready(function() {
   $("form#input").submit(function(event){
-    event.preventDefault()
+    event.preventDefault();
     const start = 0
     const end =  parseInt($("#number").val());
-    let result = range(start, end)
-    for (i = 0; i < result.length; i++)
-    if (result[i] == 3)
-    result[i] = "Won't you be my neighbor?"
-    for (i = 0; i < result.length; i++)
-    if (result[i] == 2)
-    result[i] = "boop"
-    for (i = 0; i < result.length; i++)
-    if (result[i] == 1)
-    result[i] = "beep"
-    numbers.push(result)
+    let result = range(start, end);
+    let result1 = result.toString();
+    for (i = 0; i < result1.length; i++) {
+    if (result1[i].includes("3")){
+      numbers[i] = "Won't you be my neighbor?";
+    } else if  (result1[i].includes("2")) {
+      numbers[i] = "boop";
+    } else if (result1[i].includes("1")) {
+      numbers[i] = "beep"
+    } else {
+      numbers[i] = result1[i];
+    }}
+    // for (i = 0; i < result.length; i++)
+    // if (result[i] == 2)
+    // result[i] = "boop"
+    // for (i = 0; i < result.length; i++)
+    // if (result[i] == 1)
+    // result[i] = "beep"
+    // numbers.push(result1)
 
     
     $("input#number").val("");
     $("#display").text(numbers);
     $("#display").show();
-  })
-})
-
-// function range(start, end) {
-//   for (var i = start; i <= end; i += 1) 
-// }
+  });
+});
