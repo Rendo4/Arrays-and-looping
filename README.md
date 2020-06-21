a6820054b4e67c1d0726e279addc7237e5ac7a3e
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-06-21 15:51:42 -0700

    filled out read me

---
+++ README.md
@@ -0,0 +1,16 @@
+# _{Arrays and Looping}_
+
+#### A project practicing arrays and looping to return set of numbers with exceptions. 06/21/2020
+
+#### By _**{Ryan Rendon}**_
+
+## Description
+
+This page will intake a user inputted number and return a sequence from 0 to the inputted number. It will replace a number containing a 1 with beep, a number containg a 2 with boop, and 3 with the phrase "won't you be my neighbor. It will also priortize 3>2>1 for example the input 31 will only return "will you be my neighbor" and not beep. 
+
+## Known Bugs
+
+At this time it does not display a warning for invalid, negative, or empty inputs. 
+
+### License
+Copyright (c) 2020 **_{Ryan Rendon}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,22 @@
-function range(start, end) {
-  return Array(end - start + 1).fill().map((_, idx) => start + idx)
-}
-
 numbers = []
 
 $(document).ready(function() {
   $("form#input").submit(function(event){
     event.preventDefault();
-    const start = 0
-    const end =  parseInt($("#number").val());
-    let result = range(start, end);
-    let result1 = result.toString();
-    for (i = 0; i < result1.length; i++) {
-    if (result1[i].includes("3")){
+    const userInput =  parseInt($("#number").val());
+    for (var i = 0; i <= userInput; i++){
+    numbers[i] = i;
+    }
+    for (var i=0; i <= userInput; i++)
+    if (numbers[i].toString().includes("3")){
       numbers[i] = "Won't you be my neighbor?";
-    } else if  (result1[i].includes("2")) {
+    } else if  (numbers[i].toString().includes("2")) {
       numbers[i] = "boop";
-    } else if (result1[i].includes("1")) {
+    } else if (numbers[i].toString().includes("1")) {
       numbers[i] = "beep"
     } else {
-      numbers[i] = result1[i];
-    }}
-    // for (i = 0; i < result.length; i++)
-    // if (result[i] == 2)
-    // result[i] = "boop"
-    // for (i = 0; i < result.length; i++)
-    // if (result[i] == 1)
-    // result[i] = "beep"
-    // numbers.push(result1)
-
-    
+      numbers[i] = numbers[i];
+    }
     $("input#number").val("");
     $("#display").text(numbers);
     $("#display").show();
