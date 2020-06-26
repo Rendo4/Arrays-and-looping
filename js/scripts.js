a37deedd22d4027d8573f74fc019e9c7f6c68a92
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-06-25 19:05:25 -0700

    resets array back to zero to show new input

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
-numbers = []
+function looping() {
+  numbers = []
+  const userInput =  parseInt($("#number").val());
+  for (var i = 0; i <= userInput; i++){
+  numbers[i] = i;
+  }
+for (var i=0; i <= userInput; i++)
+  if (numbers[i].toString().includes("3")){
+    numbers[i] = "Won't you be my neighbor?";
+  } else if  (numbers[i].toString().includes("2")) {
+    numbers[i] = "boop";
+  } else if (numbers[i].toString().includes("1")) {
+    numbers[i] = "beep"
+  } else {
+    numbers[i] = numbers[i];
+  }
+}
 
-$(document).ready(function() {
+  $(document).ready(function() {
   $("form#input").submit(function(event){
     event.preventDefault();
-    const userInput =  parseInt($("#number").val());
-    for (var i = 0; i <= userInput; i++){
-    numbers[i] = i;
-    }
-    for (var i=0; i <= userInput; i++)
-    if (numbers[i].toString().includes("3")){
-      numbers[i] = "Won't you be my neighbor?";
-    } else if  (numbers[i].toString().includes("2")) {
-      numbers[i] = "boop";
-    } else if (numbers[i].toString().includes("1")) {
-      numbers[i] = "beep"
-    } else {
-      numbers[i] = numbers[i];
-    }
+    const result = looping()
     $("input#number").val("");
     $("#display").text(numbers);
     $("#display").show();
   });
+  numbers = []
 });
